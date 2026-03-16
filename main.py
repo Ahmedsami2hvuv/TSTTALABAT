a17d15efde22e376f2b83cef202e71003194bb5e
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-03-16 18:12:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ def _parse_hour_min(env_key: str, default_hour: str, default_min: str) -> tuple:
     m = int(os.getenv(env_key.replace("HOUR", "MINUTE"), default_min))
     return h, m
 
-_report_h, _report_m = _parse_hour_min("REPORT_DAILY_HOUR", "18", "0")
-_reset_h, _reset_m = _parse_hour_min("RESET_DAILY_HOUR", "18", "10")
+_report_h, _report_m = _parse_hour_min("REPORT_DAILY_HOUR", "18", "20")
+_reset_h, _reset_m = _parse_hour_min("RESET_DAILY_HOUR", "18", "23")
 REPORT_DAILY_HOUR = _report_h
 REPORT_DAILY_MINUTE = _report_m
 RESET_DAILY_HOUR = _reset_h
